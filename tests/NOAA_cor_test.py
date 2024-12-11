360fc976049c2a60a4504ac386e15db9806d1654
Artem <poznyshev.a.a@yandex.ru>
2024-12-11 23:04:05 +0400

    Добавил геокодер оупен сорс

--- tests/NOAA_cor_test.py
+++ tests/NOAA_cor_test.py
@@ -4,6 +4,9 @@ from cores.core_OpenWeathermap.translator import translate_from_language
 from cores.core_OpenWeathermap.change import *
 from config_pack.configs import API_WEATHER
 
+from cores.coordinate_data import *
+
+
 async def run_tests():
 
 
@@ -13,11 +16,15 @@ async def run_tests():
     print(city_name_eng)
 
     # 1. Получаем координаты города
-    latitude, longitude, country_code = await get_country_coordinate(API_WEATHER, city_name_eng, input_limit=1)
-    print(latitude, longitude,)
+    # latitude, longitude, country_code = await get_country_coordinate(API_WEATHER, city_name_eng, input_limit=1)
+    # print(latitude, longitude,)
 
-    pogoda = await get_hourly_forecast(latitude, longitude)
-    print(pogoda)
+    jsson_ = await decode_address(city_name_eng)
+
+
+
+    # pogoda = await get_hourly_forecast(latitude, longitude)
+    # print(pogoda)
 
 if __name__ == "__main__":
     asyncio.run(run_tests())
